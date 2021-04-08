617903f1131fce8522c5d3e3a7c56e51b99725c6
brianhulbert23 <79952989+brianhulbert23@users.noreply.github.com>
2021-04-08 15:26:57 -0400

    Added the function into the main page

--- main_page.php
+++ main_page.php
@@ -11,5 +11,30 @@ $description = "";
 $addToCart = "";
 do_html_header($title, $header, $description);
 purchase_product($cost,$description,$addToCart);
-do_html_footer();
+
+	$cost = $_POST['cost'];
+	$description = $_POST['description'];
+	$addToCart = $_POST['addToCart'];
+ 
+		if(ISSET($addToCart)) {
+			echo "You must press the Add to Cart button in order to place your order."
+		
+		}
 ?>
+	 <li>
+         	<div class = "product_card1">
+				<h1></h1>
+				<p id ="cost" name = "cost">$29.99</p>
+				<p name = "description">Some text about the product</p>
+				<p><input id = "product1" = type = "submit" value = "Add to Cart" name = "addToCart"></p>
+			</div>
+      </li>
+
+      <li>
+         <!-- ... -->
+      </li>
+   </ul> <!-- cd-cart-items -->
+   ?>
+   
+<?php 
+do_html_footer();
