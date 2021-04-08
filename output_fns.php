8508be4d03dd5742b3dd56170efe36c01f8766ff
brianhulbert23 <79952989+brianhulbert23@users.noreply.github.com>
2021-04-08 15:09:26 -0400

    Fixed structure

--- output_fns.php
+++ output_fns.php
@@ -90,20 +90,23 @@ function side_cart(){
  </div>
  
  <?php
+ 
  function purchase_product() {
 	$cost = $_POST['cost'];
 	$description = $_POST['description'];
-	$addToCart = $_POST['addToCart1'];
-
+	$addToCart = $_POST['addToCart'];
+ 
 		if(ISSET($addToCart)) {
-			echo "You must press the Add to cart button in order to places your order"
-
+			echo "You must press the Add to Cart button in order to place your order."
+		
+		}
+?>
 	 <li>
          	<div class = "product_card1">
 				<h1></h1>
 				<p id ="cost" name = "cost">$29.99</p>
 				<p name = "description">Some text about the product</p>
-				<p><input id = "product1" = type = "submit" value = "Add to Cart" name = "addToCart1"></p>
+				<p><input id = "product1" = type = "submit" value = "Add to Cart" name = "addToCart"></p>
 			</div>
       </li>
 
@@ -111,8 +114,6 @@ function side_cart(){
          <!-- ... -->
       </li>
    </ul> <!-- cd-cart-items -->
-		}
-   ?>
 
    <div class="cd-cart-total">
       <p>Total<span>$39.96</span></p>
