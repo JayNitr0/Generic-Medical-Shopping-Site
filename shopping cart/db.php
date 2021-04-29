6a17f965e91cc91b82feceda9c4361625fbeedfa
jiazhang46 <80067323+jiazhang46@users.noreply.github.com>
2021-04-28 22:27:12 -0400

    Update db.php

--- shopping cart/db.php
+++ shopping cart/db.php
@@ -2,7 +2,7 @@
 
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","allphptricks");
+$con = mysqli_connect("localhost","root","","medical_database");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
