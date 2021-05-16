42557817ff69ff93fbfeac3d73561511f2dcd65c
Sandy J. Zheng <Jz6087@gmail.com>
2021-05-16 19:32:28 -0400

    Minor Edits

--- registration_verification.php
+++ registration_verification.php
@@ -4,37 +4,28 @@ require_once('generic_fns.php');
 
   $username=$_POST['username'];
   $userage=$_POST['age'];
-  $emailaddress=$_POST['email_address'];
-  $confirm_emailaddress=$_POST['confirm_emailaddress'];
-  $phonenumber=$_POST['phone_number'];
+  $emailaddress=$_POST['email'];
+  $phonenumber=$_POST['phonenumber'];
   $homeaddress=$_POST['home_address'];
   $password=$_POST['password'];
   $confirm_password=$_POST['confirm_password'];
   try   {
   
     //age does not pass threshold criteria 
-    if ($userage >= 18 && $userage <= 100) {
+    if ($userage <= 18 && $userage >= 100) {
       throw new Exception('please enter a valid age');
     }
 
-    //confirms that the email input matches
-    if ($emailaddress != $confirm_emailaddress) {
-      throw new Exception('The email address provided does not match - please try again.');
-    }
-
     //records home address of the client
-    if (preg_match('/^\\d+ [a-zA-Z ]+, \\d+ [a-zA-Z ]+, [a-zA-Z ]+$/', $homeaddress)
-
-    // Email Address not the same
-    if ($emailaddress != $confirm_emailaddress) {
-      throw new Exception('The Email address you entered do not match - please go back and try again.');
+    if (preg_match('/^\\d+ [a-zA-Z ]+, \\d+ [a-zA-Z ]+, [a-zA-Z ]+$/', $homeaddress)){
+      echo "";
     }
 
     // passwords not the same
     if ($password != $confirm_password) {
       throw new Exception('The passwords you entered do not match - please go back and try again.');
     }
-    //formates phonenumber of input
+    //formats phonenumber of input
     if (preg_match( '/^\+\d(\d{3})(\d{3})(\d{4})$/', $phonenumber, $matches)) {
       $phonenumber = $matches[1] . '-' .$matches[2] . '-' . $matches[3];
     }
